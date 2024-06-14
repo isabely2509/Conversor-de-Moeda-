a4b082638824b45df20609f83d28865f8db46b28
Isa <izamperim@gmail.com>
2024-06-14 07:53:14 -0300

    firts commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#janela 
+#titulo 
+#campos para selecionar as moedas de origem e destino 
+#botão para coverter 
+#lista de exibição com os nomes das moedas 
+import customtkinter
+
+
+#importar biblioteca que vai fazer a janea
+
+#criar e configurar a janela
+
+#criar os botões, textos e demais elementos 
+
+#colocar os elementos criados na tela 
+
+#rodar janela 
